410db64cd02343211fa645990c558cf7eb8389f6
Amit4218 <amitbhagat621@gmail.com>
2026-03-26 19:12:28 +0530

    removed net_cli_server

--- net_cli_server/src/helpers/cli_helper.py
+++
@@ -1,8 +0,0 @@
-import questionary
-from typing import List
-
-
-def ask_for_user_choice(items: List[str], message: str) -> None:
-    choices = [questionary.Choice(title=item, value=i) for i, item in enumerate(items)]
-    choice = questionary.select(message, choices=choices).ask()
-    return choice
--- net_cli_server/src/utils/constants.py
+++
@@ -1,8 +0,0 @@
-from queue import Queue
-
-SOAP_BASE_URL = "https://ww3.soap2dayhdz.com"
-SOAP_IMAGE_PREFIX = "https://img.icdn.my.id/thumb/w_156/h_234/"
-
-
-# For link storage and music playing
-MOVIE_PLAYER = Queue()
